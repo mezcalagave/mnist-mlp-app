e5758664cc501807dd0b767965c73b6dd3bebbec
mezcalagave <68863637+mezcalagave@users.noreply.github.com>
2020-11-19 17:42:40 +0900

    Add files via upload

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -79,3 +79,16 @@ test_image /= 255
 test_image = test_image.reshape(1, img_width * img_height)
 result = model.predict(test_image, batch_size=1)
 print(np.argmax(result))
+
+def load_keras_model():
+   model = load_model('models') # from ./models directory
+   return model
+
+def predict_number(model, img, width, height):
+   test_image = image.img_to_array(img)
+   test_image = test_image.astype('float32')
+   test_image = test_image.reshape(width, height)
+   test_image /= 255
+   test_image = test_image.reshape(1, width * height)
+   result = model.predict(test_image, batch_size=1)
+   return np.argmax(result)
